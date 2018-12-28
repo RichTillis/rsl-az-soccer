63fcdf3bb72a15a2ab44938ae994440e00accc58
Rich <rich.tillis@gmail.com>
2018-12-28 16:53:25 -0700

    Tournament Standings page - initial commit

--- src/app/pages/standings/standings.page.ts
+++ src/app/pages/standings/standings.page.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import * as _ from "lodash";
+
+import { TournamentService } from "../../services/tournament.service";
 
 @Component({
-  selector: 'app-standings',
-  templateUrl: './standings.page.html',
-  styleUrls: ['./standings.page.scss'],
+  selector: "app-standings",
+  templateUrl: "./standings.page.html",
+  styleUrls: ["./standings.page.scss"]
 })
 export class StandingsPage implements OnInit {
+  private allTeams: any;
+  private allTeamDivisions: any;
+  teams = [];
+  queryText: string;
+  standings: any[];
 
-  constructor() { }
+  constructor(private tournamentService: TournamentService) {
+    tournamentService.getTournamentData(68462).subscribe(data => {
+      console.log(data);
+      this.allTeams = data.teams;
+      this.allTeamDivisions = _.chain(data.standings)
+        .groupBy("flight")
+        .toPairs()
+        .map(item => _.zipObject(["divisionName", "divisionTeams"], item))
+        .value();
 
-  ngOnInit() {
+      this.teams = this.allTeamDivisions
+      console.log("teams", this.teams);
+    });
   }
 
+  ngOnInit() {
+    // let tourneyData = this.tournamentService.getTournamentData(68462);
+    // console.log(tourneyData);
+    // this.standings = tourneyData.standings;
+    // console.log(this.standings);
+  }
 }
