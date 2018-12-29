60f5b87876edd1281db9b3b34fffa11318aa28c1
Rich <rich.tillis@gmail.com>
2018-12-28 17:10:48 -0700

    code cleanup and minor css addition

--- src/app/pages/standings/standings.page.ts
+++ src/app/pages/standings/standings.page.ts
@@ -9,31 +9,19 @@ import { TournamentService } from "../../services/tournament.service";
   styleUrls: ["./standings.page.scss"]
 })
 export class StandingsPage implements OnInit {
-  private allTeams: any;
-  private allTeamDivisions: any;
+  TOURNAMENT_ID = 68462;
   teams = [];
-  queryText: string;
-  standings: any[];
 
   constructor(private tournamentService: TournamentService) {
-    tournamentService.getTournamentData(68462).subscribe(data => {
-      console.log(data);
-      this.allTeams = data.teams;
-      this.allTeamDivisions = _.chain(data.standings)
+    tournamentService.getTournamentData(this.TOURNAMENT_ID).subscribe(data => {
+      this.teams = _.chain(data.standings)
         .groupBy("flight")
         .toPairs()
         .map(item => _.zipObject(["divisionName", "divisionTeams"], item))
         .value();
-
-      this.teams = this.allTeamDivisions
-      console.log("teams", this.teams);
+      // console.log("teams", this.teams);
     });
   }
 
-  ngOnInit() {
-    // let tourneyData = this.tournamentService.getTournamentData(68462);
-    // console.log(tourneyData);
-    // this.standings = tourneyData.standings;
-    // console.log(this.standings);
-  }
+  ngOnInit() {}
 }
